4717f019f02f102c5faf1a2b2fc18130f1425729
yulduz <yulduz83@gmail.com>
2021-03-25 12:25:33 -0500

    fixes broken route

--- public/javascript/signup-questionnaire.js
+++ public/javascript/signup-questionnaire.js
@@ -20,7 +20,7 @@ async function signupQuestionnaireFormHandler(event) {
     headers: { "Content-Type": "application/json" },
   });
   if (response.ok) {
-    document.location.replace("/profile/bio");
+    document.location.replace("/profile/more-info");
   } else {
     alert(response.statusText);
   }
