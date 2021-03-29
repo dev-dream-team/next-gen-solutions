adf6fadf3b0a67b40627cd0dbd48be03ea58b807
yulduz <yulduz83@gmail.com>
2021-03-29 02:14:06 -0500

    cleanup: adds missing user profile refs to seeds

--- seeds/user-interests-seeds.js
+++ seeds/user-interests-seeds.js
@@ -1,16 +1,21 @@
-const { UserInterest } = require('../models');
+const { UserInterest } = require("../models");
 
 const userInterestData = [
-    {
-        id: 1,
-        user_profile_id: 1,
-        interest_id: 2
-    },
-    {
-        id: 2,
-        user_profile_id: 1,
-        interest_id: 6
-    }
+  {
+    id: 1,
+    user_profile_id: 1,
+    interest_id: 2,
+  },
+  {
+    id: 2,
+    user_profile_id: 2,
+    interest_id: 6,
+  },
+  {
+    id: 3,
+    user_profile_id: 3,
+    interest_id: 4,
+  },
 ];
 
 const seedUserInterests = () => UserInterest.bulkCreate(userInterestData);
